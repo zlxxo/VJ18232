b15a4cb5c27a35ae1680a5ccb6e13ab5afbc4e04
zlxxo <mramic2@etf.unsa.ba>
2019-03-21 16:15:02 +0100

    prvi

---
+++ app/src/main/java/ba/unsa/etf/rma/VJ_18232/MainActivity.java
@@ -0,0 +1,45 @@
+package ba.unsa.etf.rma.VJ_18232;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button dugme;
+    private ListView lista;
+    private EditText tekst;
+    private ArrayAdapter<String> adapter = null;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dugme = (Button)findViewById(R.id.button);
+        lista = (ListView)findViewById(R.id.listView);
+        tekst = (EditText)findViewById(R.id.editText);
+
+        final ArrayList<String> unosi = new ArrayList<String>();
+
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, unosi);
+
+        lista.setAdapter(adapter);
+
+        dugme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                unosi.add(0,tekst.getText().toString());
+                adapter.notifyDataSetChanged();
+                tekst.setText("");
+            }
+        });
+    }
+}
